f549bb15ee2a4a0857182d0240f20480b033fc4b
Seher <seherakyl06@gmail.com>
2025-05-23 00:13:22 +0300

    Add user model with database connection and retrieval functions

---
+++ models/users.py
@@ -0,0 +1,69 @@
+
+from multiprocessing import connection
+import mysql.connector
+from mysql.connector import Error
+
+CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '123456789',
+    'database': 'food_choice'
+}
+def is_db_connected():
+    connection = mysql.connector.connect(**CONFIG)
+    if not connection:
+        print("baglanti basarisiz")
+    else:
+        print("baglanti basarili")
+is_db_connected()
+
+
+
+#delete_users_by_id(users_id)
+#update_users(users_id,user_name,email,password,is_active)
+#is_active()
+#Users_login(email,password)
+#Register_user(user_name,mail,password)
+
+def get_users_full_info_by_id(users_id):
+    connection=mysql.connector.connect(**CONFIG)
+    if not connection:
+        return None
+    try:
+        cursor=connection.cursor(dictionary=True)
+        query="SELECT * FROM digital_library.users WHERE id=%s"
+        cursor.execute(query,(users_id,))  
+        user=cursor.fetchone()
+        return user
+    
+    except Exception as e:
+        print("kullanici getirelemedi")
+        print(f"hata:{e}")
+        return 
+    finally:
+        cursor.close()
+        connection.close()
+#print(get_users_full_info_by_id(1))
+
+
+
+def get_users_by_user_name(users_name):
+    connection=mysql.connector.connect(**CONFIG)
+    if not connection:
+        return None
+    try:
+        cursor=connection.cursor(dictionary=True)
+        query="SELECT user_name FROM digital_library.users WHERE user_name=%s"
+        cursor.execute(query,(users_name,))  
+        users=cursor.fetchone()
+        return users
+    
+    except Exception as e:
+        print("kullanici getirilemedi")
+        print(f"hata:{e}")
+        return None
+    
+    finally:
+        cursor.close()
+        connection.close()
+#print(get_users_by_user_name("elif"))
