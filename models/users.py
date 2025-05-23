1f85b2fb0f81515a9b5ee660b4de7cbb128dfbe5
Seher <seherakyl06@gmail.com>
2025-05-23 10:10:01 +0300

    Refactor user management functions: update_users now correctly updates user details, added is_active and Users_login functions for user status check and authentication.

--- models/users.py
+++ models/users.py
@@ -20,9 +20,6 @@ is_db_connected()
 
 
 
-#is_active()
-#Users_login(email,password)
-#Register_user(user_name,mail,password)
 
 def get_users_full_info_by_id(users_id):
     connection=mysql.connector.connect(**CONFIG)
@@ -90,6 +87,7 @@ def delete_users_by_id(users_id):
 
 
 
+# ömere sor 
 def update_users(users_id,user_name,email,password,is_active):
     connection=mysql.connector.connect(**CONFIG)
     if not connection:
@@ -101,7 +99,7 @@ def update_users(users_id,user_name,email,password,is_active):
             SET user_name = %s,email=%s,password=%s,is_active=%s
             WHERE id = %s
         """
-        cursor.execute(query,(users_id,user_name,email,password,is_active))
+        cursor.execute(query,(user_name,email,password,is_active,users_id))
         connection.commit()
         print(f"{user_name} kullanisi güncellendi")
 
@@ -112,4 +110,67 @@ def update_users(users_id,user_name,email,password,is_active):
     finally:
         cursor.close()
         connection.close()
-print(update_users(4,"a","b.@mail.com","123",1))
+#print(update_users("a","b.@mail.com","123",1,3))
+
+
+
+
+def is_active(users_id):
+    connection = mysql.connector.connect(**CONFIG) 
+    if not connection: 
+        return None
+    try: 
+        cursor=connection.cursor(dictionary=True)
+        query="SELECT * FROM digital_library.users WHERE id=%s"
+        cursor.execute(query,(users_id,))  
+        users=cursor.fetchone()
+
+        if users and users["is_active"] == 1:
+            print("kullanici aktif")
+        else:
+            print("kullanici aktif degildir")
+
+    except Exception as e:
+        print(f"hata:{e}")
+        return None
+    finally:
+        cursor.close()
+        connection.close()
+#print(is_active(3))
+
+
+
+def Users_login(email,password):
+    connection=mysql.connector.connect(**CONFIG)
+    if not connection:
+        return None
+    try:
+        cursor=connection.cursor(dictionary=True)
+        query = "SELECT * FROM digital_library.users WHERE email = %s AND password = %s"
+        cursor.execute(query, (email, password))
+        result = cursor.fetchall()
+        if result:
+            Users=result[0]
+            print(f"giris basarili:hos geldin {Users['email']}")
+            return Users
+        else:
+            print("hatali kullanici adi veya şifre")
+            return None
+    except mysql.connector.Error as db_err:
+            print(f"Veritabanı işlem hatası: {db_err}")
+            return None
+    except mysql.connector.Error as conn_err:
+            print(f"Veritabanı bağlantı hatası: {conn_err}")
+            return None
+    except Exception as e:
+            print(f"Beklenmeyen hata: {e}")
+            return None
+    finally:
+        cursor.close()
+        connection.close()
+#print(Users_login("elif@mail.com","hashed_password3"))
+
+
+
+
+def Register_users(user_name,mail,password):
