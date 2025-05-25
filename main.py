6f9f18f8d9b547992f6334f183daf19d0706380a
Seher <seherakyl06@gmail.com>
2025-05-25 22:19:22 +0300

    Add ratings routes to main.py and include router for ratings

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 from fastapi import FastAPI
 from routes.users_routes import router as users_routes
 from routes.books_routes import router as books_routes
+from routes.ratings_routes import router as ratings_routes
 app = FastAPI()
 
 
 app.include_router(users_routes,prefix="/users",tags=["Users"])
 app.include_router(books_routes,prefix="/books",tags=["Books"])
+app.include_router(ratings_routes,prefix="/ratings",tags=["Ratings"])
